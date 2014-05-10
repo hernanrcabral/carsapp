93b075cbfa9377502702cda0c6731dc6521e54c1
Damaris <damaris_444@hotmail.com>
2014-05-09 23:00:22 -0300

    SRS

--- config/schema.sql
+++ config/schema.sql
@@ -19,4 +19,29 @@ create table cities(
 	constraint pk_city primary key (id)
 );
 
-
+DROP TABLE IF EXISTS vehicles; 	-- Vehiculos
+CREATE TABLE vehicles(
+	patent VARCHAR (11) not null, 
+	model VARCHAR(11) not null,
+	mark  VARCHAR(11) not null,
+    descripcion VARCHAR (40),
+    type enum('Car','Truck','Motocycle','Others') not null,
+   	price FLOAT not null,
+	status enum('Sell','Rent') not null,
+	user_id integer(11) not null references users(id),
+	city_id int(11) not null references cities(id),
+	constraint pk_vehicle primary key (patent)
+);	
+drop table if exists buildings;
+create table buildings(
+	id integer(100) not null auto_increment,
+	type enum('Land','Farm','House','Departament','Garage') not null,	
+	owner_id integer(100) not null references owners(id),
+	real_estate_id integer(100) not null references real_estates(id),
+	description varchar(400),
+	price float not null,
+	status enum('Sell','Rent') not null,
+	street varchar(45) not null,
+	neighborhood varchar(45) not null,
+	city_id int(11) not null references cities(id),
+	constraint pk_buildings primary key (id)
