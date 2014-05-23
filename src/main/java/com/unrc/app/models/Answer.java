7be2aa288c7eefad78214eaba890da9474ef8bd0
Damaris <damaris_444@hotmail.com>
2014-05-23 03:31:05 -0300

    Validation of the class Answer

---
+++ src/main/java/com/unrc/app/models/Answer.java
@@ -0,0 +1,10 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+public class Answer extends Model {
+	static{
+		validatePresenceOf("user_id","description","id_answer");
+	}
+}
+
