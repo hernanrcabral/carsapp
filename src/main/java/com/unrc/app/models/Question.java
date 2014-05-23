5180ddf694e12f8f22a7b1a7b14e1459fc746e1b
Silvia <silvia@ubuntu.ubuntu-domain>
2014-05-23 04:03:47 -0300

    Correccion de la validacion

--- src/main/java/com/unrc/app/models/Question.java
+++ src/main/java/com/unrc/app/models/Question.java
@@ -2,9 +2,9 @@ package com.unrc.app.models;
 
 import org.javalite.activejdbc.Model;
 
-public class Vehicle extends Model {
+public class Question extends Model {
 	static{
-		validatePresenceOf("user_id", "id_question""description","id_answer");
+		validatePresenceOf("user_id", "id_question","description","id_answer");
 	}
 }
 
