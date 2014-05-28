7a5224628c1e22943751b6a7c6754b83954c0a2c
Damaris <damaris_444@hotmail.com>
2014-05-28 03:27:39 -0300

    Updating Truck.java

---
+++ src/main/java/com/unrc/app/models/Truck.java
@@ -0,0 +1,10 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+public class Truck extends Model {
+	static{
+		validatePresenceOf("id","count_belt","id_vehicle");
+	}
+}
+
