683bae2dda8cb1cf569f387f42be15e953032891
Damaris <damaris_444@hotmail.com>
2014-06-21 23:59:15 -0300

    VehicleTest.java Corregido

--- src/test/java/com/unrc/app/VehicleTest.java
+++ src/test/java/com/unrc/app/VehicleTest.java
@@ -9,7 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.javalite.test.jspec.JSpec.the;
-import static org.junit.Assert.assertEquals;													
+import static org.junit.Assert.assertEquals;                                                    
 
 
 public class VehicleTest{
@@ -22,20 +22,24 @@ public class VehicleTest{
 
     @After
     public void after(){
-        System.out.println("VehicleTest tearDown");				
+        System.out.println("VehicleTest tearDown");             
         Base.rollbackTransaction();
         Base.close();
     }
 
     @Test
     public void shouldValidateMandatoryFields(){
-       Vehicle vehicle= new Vehicle();
-
+        Vehicle vehicle= new Vehicle();
+        User user = new User();
+        
+        user.set("id",1,"first_name", "John", "last_name", "Doe", "email", "example@email.com");
+        user.save();
+       
        // check errors
         the(vehicle).shouldNotBe("valid");
         the(vehicle.errors().get("patent")).shouldBeEqual("value is missing");
         the(vehicle.errors().get("kind")).shouldBeEqual("value is missing");
-		the(vehicle.errors().get("mark")).shouldBeEqual("value is missing");
+        the(vehicle.errors().get("mark")).shouldBeEqual("value is missing");
         the(vehicle.errors().get("description")).shouldBeEqual("value is missing");
         the(vehicle.errors().get("status")).shouldBeEqual("value is missing");  
         the(vehicle.errors().get("price")).shouldBeEqual("value is missing");
@@ -43,16 +47,13 @@ public class VehicleTest{
         the(vehicle.errors().get("city_id")).shouldBeEqual("value is missing");
 
 
-        User user = new User();
-        user.set("id",1,"first_name", "John", "last_name", "Doe", "email", "example@email.com");
-        user.save();
-
-		// Create Vehicle
-    	 vehicle.set("patent", "HDK526", "kind", "307", "mark", "Fiat", "description", "Combustible: Diesel, Color: Negro. Sin uso. Muy pocos kilómetros. Excelente estado. Alarma. Papeles al día. Listo para transferir. ", "status", "Sell", "price", 32.393, "user_id", user.get("id"), "city_id", 1);
-
+        // Create Vehicle
+        vehicle.set("patent", "HDK526", "kind", "307", "mark", "Fiat", "description", "Combustible: Diesel, Muy pocos kilómetros. Excelente estado.. ", "status", "Sell", "price", 32.393, "user_id", user.get("id"), "city_id", 1);
         vehicle.save();
 
-		System.out.println(vehicle);
+//        vehicle.setParent(user);
+
+        System.out.println(vehicle);
         System.out.println(vehicle.parent(User.class));
 
         // Everything is good:
@@ -60,4 +61,5 @@ public class VehicleTest{
 
     }
 }
+
 
