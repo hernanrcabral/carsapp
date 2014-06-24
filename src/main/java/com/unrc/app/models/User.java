aa4932fb3905679506639538e72938df473a5578
Damaris <damaris_444@hotmail.com>
2014-06-24 15:13:32 -0300

    User.java Actualizado Moustache

--- src/main/java/com/unrc/app/models/User.java
+++ src/main/java/com/unrc/app/models/User.java
@@ -6,5 +6,18 @@ public class User extends Model {
  static {
       validatePresenceOf("first_name", "last_name","email");
   }
+  
+  	public String email (){
+  		return this.getString("email");
+ 	}
+
+	public String id (){
+  		return String.valueOf(this.getInteger("id"));
+  	}
+
+  	public String first_name (){
+  		return this.getString("first_name");
+  	}
+
 }
 
