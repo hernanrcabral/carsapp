3a182891b98e9643e98c613f80c38643fba6ad68
Damaris <damaris_444@hotmail.com>
2014-06-24 15:24:12 -0300

    city.java Actualizado

--- src/main/java/com/unrc/app/models/City.java
+++ src/main/java/com/unrc/app/models/City.java
@@ -6,5 +6,19 @@ public class City extends Model{
 	static{
 		validatePresenceOf("postal_code", "name");
 	}
+
+  	public String name (){
+  		return this.getString("name");
+ 	}
+
+	public String id (){
+  		return String.valueOf(this.getInteger("id"));
+  	}
+
+  	public String postal_code (){
+  		return String.valueOf(this.getInteger("postal_code"));
+  	}
+
+
 }
 
