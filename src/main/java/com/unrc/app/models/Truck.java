c40cc2ee961385da732f91da88391eebe38f8fb9
Damaris <damaris_444@hotmail.com>
2014-06-26 13:09:25 -0300

    Truck.java Validacion

--- src/main/java/com/unrc/app/models/Truck.java
+++ src/main/java/com/unrc/app/models/Truck.java
@@ -4,7 +4,43 @@ import org.javalite.activejdbc.Model;
 
 public class Truck extends Model {
 	static{
-		validatePresenceOf("id","count_belt","id_vehicle");
+		validatePresenceOf("count_belt");
 	}
+
+		public String count_belt (){
+		String cant = "No Ingreso la cantidad de cinturones.";
+
+		if(this.getString("count_belt").charAt(0)=='1'){
+            cant="1";
+        }
+        if(this.getString("count_belt").charAt(0)=='2'){
+           	cant="2";
+        } 
+        if(this.getString("count_belt").charAt(0)=='3'){
+       		cant="3";
+        }
+       	if(this.getString("count_belt").charAt(0)=='4'){
+        	cant="4";
+       	}
+       	if(this.getString("count_belt").charAt(0)=='5'){
+         	cant="5";
+       	}
+       	if(this.getString("count_belt").charAt(0)=='6'){
+        	cant="6";
+       	}
+       	if(this.getString("count_belt").charAt(0)=='7'){
+        	cant="Mas de 6 cinturones";
+       	}
+       	
+     	
+  		return cant;
+  	}
+	public String id (){
+  		return String.valueOf(this.getInteger("id"));
+  	}
+
+	public String id_vehicle (){
+  		return this.getString("id_vehicle");
+  	}
 }
 
