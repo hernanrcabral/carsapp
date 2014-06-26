ad98736b1532bf4a860f9f8dfd310c8c116114d1
Damaris <damaris_444@hotmail.com>
2014-06-26 13:18:47 -0300

    CarTest.java

--- src/test/java/com/unrc/app/CarTest.java
+++ src/test/java/com/unrc/app/CarTest.java
@@ -1,7 +1,6 @@
 package com.unrc.app;
 
 import com.unrc.app.models.Car;
-import com.unrc.app.models.User;
 
 import org.javalite.activejdbc.Base;
 import org.junit.After;
@@ -30,19 +29,13 @@ public class CarTest{
     @Test
     public void shouldValidateMandatoryFields(){
         Car car= new Car();
-        User user = new User();
-        
-        user.set("id",1,"first_name", "John", "last_name", "Doe", "email", "example@email.com");
-        user.save();
      
        // check errors
         the(car).shouldNotBe("valid");
-        the(car.errors().get("id_vehicle")).shouldBeEqual("value is missing");
         the(car.errors().get("count_doors")).shouldBeEqual("value is missing");
-        the(car.errors().get("id")).shouldBeEqual("value is missing");
 
         // Create Vehicle
-        car.set("id",1, "id_vehicle", 1, "count_doors", 4);
+        car.set("id_vehicle", "ASD123", "count_doors", 4);
         car.save();
         
         System.out.println(car);
