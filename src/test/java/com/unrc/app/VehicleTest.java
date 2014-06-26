7238c17d88ccae5ddb7b68d6e2d2f8c44e4ff5d4
Damaris <damaris_444@hotmail.com>
2014-06-26 13:24:42 -0300

    VehicleTest.java Datos Concretos

--- src/test/java/com/unrc/app/VehicleTest.java
+++ src/test/java/com/unrc/app/VehicleTest.java
@@ -1,6 +1,5 @@
 package com.unrc.app;
 
-import com.unrc.app.models.User;
 import com.unrc.app.models.Vehicle;
 
 import org.javalite.activejdbc.Base;
@@ -30,31 +29,20 @@ public class VehicleTest{
     @Test
     public void shouldValidateMandatoryFields(){
         Vehicle vehicle= new Vehicle();
-        User user = new User();
-        
-        user.set("id",1,"first_name", "John", "last_name", "Doe", "email", "example@email.com");
-        user.save();
        
        // check errors
         the(vehicle).shouldNotBe("valid");
         the(vehicle.errors().get("patent")).shouldBeEqual("value is missing");
         the(vehicle.errors().get("kind")).shouldBeEqual("value is missing");
         the(vehicle.errors().get("mark")).shouldBeEqual("value is missing");
-        the(vehicle.errors().get("description")).shouldBeEqual("value is missing");
-        the(vehicle.errors().get("status")).shouldBeEqual("value is missing");  
-        the(vehicle.errors().get("price")).shouldBeEqual("value is missing");
-        the(vehicle.errors().get("user_id")).shouldBeEqual("value is missing");
-        the(vehicle.errors().get("city_id")).shouldBeEqual("value is missing");
-
 
         // Create Vehicle
-        vehicle.set("patent", "HDK526", "kind", "307", "mark", "Fiat", "description", "Combustible: Diesel, Muy pocos kilómetros. Excelente estado.. ", "status", "Sell", "price", 32.393, "user_id", user.get("id"), "city_id", 1);
+        vehicle.set("patent", "HDK526", "kind", "307", "mark", "Fiat","user_id", "nuevo@gmail.com", "city_id","Rosario");
         vehicle.save();
 
 //        vehicle.setParent(user);
 
         System.out.println(vehicle);
-        System.out.println(vehicle.parent(User.class));
 
         // Everything is good:
         the(vehicle).shouldBe("valid");
