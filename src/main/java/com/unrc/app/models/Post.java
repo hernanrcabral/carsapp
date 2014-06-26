c89041d5553d91ccb87ea809c5b7a600ce0ece01
Damaris <damaris_444@hotmail.com>
2014-06-26 13:08:25 -0300

    Post.java Validacion

--- src/main/java/com/unrc/app/models/Post.java
+++ src/main/java/com/unrc/app/models/Post.java
@@ -4,7 +4,24 @@ import org.javalite.activejdbc.Model;
 
 public class Post extends Model {
 	static{
-		validatePresenceOf("description","vehicle_id", "user_id");
+		validatePresenceOf("description");
 	}
+
+	public String id (){
+  		return String.valueOf(this.getInteger("id"));
+	}
+
+  	public String user_id (){
+  		return this.getString("user_id");
+ 	}
+
+  	public String vehicle_id (){
+  		return this.getString("vehicle_id");
+  	}
+	
+	public String description (){
+  		return this.getString("description");
+  	}
+
 }
 
