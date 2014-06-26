21621a0ecf8e74c042e311a301d04d462ae883d4
Damaris <damaris_444@hotmail.com>
2014-06-26 13:07:32 -0300

    Motocicle.java Validacion

--- src/main/java/com/unrc/app/models/Motocicle.java
+++ src/main/java/com/unrc/app/models/Motocicle.java
@@ -4,7 +4,19 @@ import org.javalite.activejdbc.Model;
 
 public class Motocicle extends Model {
 	static{
-		validatePresenceOf("id","cylinder","id_vehicle");
+		validatePresenceOf("cylinder");
 	}
+
+	public String cylinder (){
+		return String.valueOf(this.getInteger("cylinder"));
+
+  	}
+	public String id (){
+  		return String.valueOf(this.getInteger("id"));
+  	}
+
+	public String id_vehicle (){
+  		return this.getString("id_vehicle");
+  	}
 }
 
