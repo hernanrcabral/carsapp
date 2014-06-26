5350ada449f87fa012ca1fa193cf9cafc4a6905f
Damaris <damaris_444@hotmail.com>
2014-06-26 13:05:09 -0300

    Answer.java

--- src/main/java/com/unrc/app/models/Answer.java
+++ src/main/java/com/unrc/app/models/Answer.java
@@ -6,5 +6,22 @@ public class Answer extends Model {
 	static{
 		validatePresenceOf("user_id","answer","question_id");
 	}
+
+	public String id (){
+  		return String.valueOf(this.getInteger("id"));
+	}
+
+  	public String user_id (){
+  		return this.getString("user_id");
+ 	}
+
+  	public String answer (){
+  		return this.getString("answer");
+  	}
+	
+	public String question_id (){
+  		return this.getString("question_id");
+  	}
+
 }
 
